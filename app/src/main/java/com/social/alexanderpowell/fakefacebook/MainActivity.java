0721efefc96b8b8395d61beb7cfbd02cafe0e30f
Alexander Powell <ajpowell@email.wm.edu>
2019-10-22 22:32:24 -0700

    updated play store graphics

--- app/src/main/java/com/social/alexanderpowell/fakefacebook/MainActivity.java
+++ app/src/main/java/com/social/alexanderpowell/fakefacebook/MainActivity.java
@@ -3,20 +3,17 @@ package com.social.alexanderpowell.fakefacebook;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView nameTextView, statusTextView;
-    //EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,43 +29,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         nameTextView = findViewById(R.id.nameTextView);
         statusTextView = findViewById(R.id.statusTextView);
 
-        //nameEditText = viewfindViewById(R.id.nameEditText);
-
         nameTextView.setOnClickListener(this);
         statusTextView.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+
+        // Set up Alert Dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
-        final Context context = builder.getContext();
-        final LayoutInflater inflater = LayoutInflater.from(context);
-        final View vieww = inflater.inflate(R.layout.dialog_name, null, false);
+        final EditText editText = new EditText(MainActivity.this);
+        editText.setHint("Enter text here");
+        editText.setInputType(InputType.TYPE_CLASS_TEXT);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        lp.setMarginStart(10);
+        lp.setMarginEnd(10);
+        editText.setLayoutParams(lp);
 
         if (view.getId() == R.id.nameTextView) {
 
-            builder.setView(R.layout.dialog_name);
-            builder.setTitle("Title");
+            builder.setView(editText);
+            builder.setTitle("Add new name");
             builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
-                            //Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_SHORT).show();
-                            EditText et = vieww.findViewById(R.id.nameEditText);
-                            showToast(et.getText().toString());
-                            nameTextView.setText(et.getText().toString());
+                            nameTextView.setText(editText.getText().toString());
                         }
                     })
                     .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
-                            //Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_SHORT).show();
+                            // Do nothing
                         }
                     });
             builder.show();
+
         } else if (view.getId() == R.id.statusTextView) {
 
-        }
-    }
+            builder.setView(editText);
+            builder.setTitle("Add new status");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_SHORT).show();
+                            statusTextView.setText(editText.getText().toString());
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // Do nothing
+                        }
+                    });
+            builder.show();
 
-    private void showToast(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        }
     }
 }
